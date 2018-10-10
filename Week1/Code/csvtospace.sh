4b019eaa151066a0673729908308de305344e4c0
KBicks <katie.bickerton@outlook.com>
2018-10-10 06:20:56 +0100

    Changes to shell scripting practical

---
+++ Week1/Code/csvtospace.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: Katie Bickerton k.bickerton18@imperial.ac.uk
+# Script: csvtospace.sh
+# Desc: change csv to space delimited files, via substitution of commas for spaces
+# Arguments: 1-> comma delimited file 
+# Date: 08 Oct 2018
+
+echo "Creating a space delimited version of $1"
+cat $1 | tr -s "," " " >> $1.txt 
+echo "Done!"
+exit
