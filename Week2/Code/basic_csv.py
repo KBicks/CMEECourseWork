a791afeffd7edccb3f7005d13902663d62642599
Katie Bickerton <katie.bickerton@outlook.com>
2018-10-11 15:40:34 +0100

    Final course files from Python part 1 and modifications to Week2 codes

--- Week2/Code/basic_csv.py
+++ Week2/Code/basic_csv.py
@@ -1,28 +1,45 @@
+#!/usr/bin/env python3
+"""Reads and prints a csv file, then writes specific components of the read csv
+to a new csv."""
+
+__author__ = "Katie Bickerton (k.bickerton18@imperial.ac.uk)"
+__version__ = "3.5.2"
+
 import csv
 
 # Read a file containing:
 # 'Species','Infrairder','Family','Distribution','Body mass'
 f = open('../Data/testcsv.csv','r')
 
-
+# Sets csv file to be read
 csvread = csv.reader(f)
+# Creates a temporary list for outputs
 temp = []
 for row in csvread:
+    # Adds each row of the input to the temp output file as a tuple, then prints
     temp.append(tuple(row))
     print(row)
+    # prints string then the first entry of each row (indexed as 0)
     print("The species is", row[0])
 
 f.close()
 
 # write a file containing only species name and Body mass
+
+# opens testcsv.csv as a file to be read and set to variable f
 f = open('../Data/testcsv.csv','r')
+# opens bodymass.csv as a file to be written and set to variable g
 g = open('../Data/bodymass.csv', 'w')
+
 
 csvread = csv.reader(f)
 csvwrite = csv.writer(g)
 for row in csvread:
+    # print each row in read csv
     print(row)
+    # write the 0 and 4 entries from each row (indexing starts at 0) into the write csv
     csvwrite.writerow([row[0], row[4]])
 
+# close both files
 f.close()
 g.close()
--- Week2/Code/basic_io.py
+++ Week2/Code/basic_io.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+"""Demonstrates inputing text files into python: 
+File Input prints first every line in a file, then every line apart from blank lines,
+File Output generates a list of a specified range, then saves it with each element on a new line,
+Storing Files using the pickle module"""
+
+__author__ = "Katie Bickerton (k.bickerton18@imperial.ac.uk)"
+__version__ = "3.5.2"
+
+
 ##############
 # FILE INPUT
 ##############
@@ -5,6 +15,7 @@
 f = open('../Sandbox/test.txt','r') #r is to read txt
 # use "implicit" for loop
 # if the object is a file, python will cycle over lines
+# prints each line of input file
 for line in f:
     print(line)
 
@@ -12,9 +23,11 @@ for line in f:
 f.close()
 
 
-#Same example, skip blank lines
+# Same example, skip blank lines
 f = open('../Sandbox/test.txt','r')
 for line in f:
+    # removes blank lines by looking for lines (after stripping trailing spaces) which have lengths > 0
+    # only lines that fulfil this condition are printed
     if len(line.strip()) > 0:
         print(line)
 
@@ -27,6 +40,7 @@ f.close()
 list_to_save = range(100)
 
 f = open('../Sandbox/testout.txt','w')
+# for each element of the list, print in file specified above and add a new line per element
 for i in list_to_save:
     f.write(str(i) + '\n') ## Add a new line at the end
 
@@ -36,18 +50,21 @@ f.close()
 # STORING OBJECTS
 ################
 # To save an object (even complex) for later use
+# Create a dictionary 
 my_dictionary = {"a key": 10, "another key": 11}
 
 import pickle
 #module that allows saving from RAM to hard disk
 
-f = open('../Sandbox/testp.p','wb') ## not the b: accept binary files
-pickle.dump(my_dictionary, f)
+f = open('../Sandbox/testp.p','wb') ## note the b: accept binary files
+# stores dictionary
+pickle.dump(my_dictionary, f) 
 f.close()
 
 ##Load the data again
 f = open('../Sandbox/testp.p','rb')
-another_dictionary = pickle.load(f)
+#loads stored dictionary 
+another_dictionary = pickle.load(f) 
 f.close()
 
 print(another_dictionary)
